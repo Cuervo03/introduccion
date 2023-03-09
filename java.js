f4ee311685a89076a5b55e98fbc8ebbb20dab3f6
Karina <nakavapi.30@gmail.com>
2023-03-09 11:13:27 -0500

    first commit

---
+++ java.js
@@ -0,0 +1,13 @@
+let dato1,dato2,num1, num2, resultado;
+dato1 = window.prompt("Favor digite el primer numero", "0");
+num1 = parseInt(dato1);
+dato2 = window.prompt("Favor digite el segundo numero", "0");
+num2 = parseInt(dato2);
+if (num1 > num2)
+{
+resultado = " el mayor es el numero uno";
+}
+else
+{
+resultado = " el mayor es el segundo numero";
+}
